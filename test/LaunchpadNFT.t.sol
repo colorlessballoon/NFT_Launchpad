7caaf2248f9a2b0f1b300c9ddc173e2be5c2310a
colorlessballoon <colorlessballoon@163.com>
2026-02-24 12:30:19 +0800

    test(nft): add mint edge case and withdraw tests

--- test/LaunchpadNFT.t.sol
+++ test/LaunchpadNFT.t.sol
@@ -22,4 +22,40 @@ contract LaunchpadNFTTest is Test {
 
         assertEq(launchpadNFT.totalSupply(), 1);
     }
+
+    function testMintRevertIfIncorrectPayment() public {
+        vm.deal(user, 1 ether);
+        vm.prank(user);
+        vm.expectRevert("Incorrect payment");
+        launchpadNFT.mint{value: 0.005 ether}(1);
+        vm.stopPrank();
+        assertEq(launchpadNFT.totalSupply(), 0);
+    }
+
+    
+    function testMintRevertIfExceedsMaxSupply() public {
+        vm.deal(user, 100 ether);
+        vm.prank(user);
+        launchpadNFT.mint{value: 1 ether}(100);
+        vm.prank(user);
+        vm.expectRevert("Max supply reached");
+        launchpadNFT.mint{value: 0.01 ether}(1);
+        assertEq(launchpadNFT.totalSupply(), 100);
+    }
+
+    function testWithdraw() public {
+        vm.deal(user, 1 ether);
+
+        vm.prank(user);
+        launchpadNFT.mint{value: 0.01 ether}(1);
+        vm.stopPrank();
+
+        uint256 ownerBalanceBefore = address(this).balance;
+        launchpadNFT.withdraw();
+        uint256 ownerBalanceAfter = address(this).balance;
+        assertGt(ownerBalanceAfter, ownerBalanceBefore);
+    }
+
+    receive() external payable {}
+    
 }
